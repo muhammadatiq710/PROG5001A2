bef30418852951141557d03f2280cdd8bb28e1cb
muhammadatiq710 <m.atiq.10@student.scu.edu.au>
2021-05-02 02:58:27 +1000

    Create Prey.java
    Declaired private and public functions with comments and inherited this class from Snake.

---
+++ Prey.java
@@ -0,0 +1,32 @@
+/**
+ * Class Prey represents any prey object.
+ *
+ * @author (Muhammad Atiq)
+ * @date (02-May-21)
+ */
+public class Prey extends Snake {
+    /**
+     * Constructor for objects of class Prey
+     */
+    public Prey()
+    {
+    }    
+    public void drawSnake(){
+    
+        }//overridden methods are always public 
+    public void moveSnake(){
+        }   //overridden methods are always public
+    public void increaseSnakeSize(){
+        }//overridden methods are always public
+    public void customizeSnakeColor(){
+            } //overridden methods are always public
+    public void drawPrey(){
+    
+        }//Method is public because we need to use it in GameBoard
+    public void displayPreyAtRandomSpots(){
+                }//Method is public because we need to use it in GameBoard
+    private void customizePreyColor(){
+                }//Method is private because we need to use it in Prey class
+       
+            
+}
